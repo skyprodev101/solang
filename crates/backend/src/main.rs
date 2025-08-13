efcf8442e5fa59d570528989862f40799270cff0
tur461 <tur461.er@gmail.com>
2025-08-13 10:49:57 +0530

    fix(crates/backend): fix cors issues on localhost
    Signed-off-by: tur461 <tur461.er@gmail.com>

--- crates/backend/src/main.rs
+++ crates/backend/src/main.rs
@@ -11,6 +11,7 @@ use actix_web::{
 };
 
 use backend::{route_compile, Opts};
+use actix_cors::Cors;
 
 pub struct FrontendState {
     pub frontend_folder: String,
@@ -51,6 +52,14 @@ async fn main() -> std::io::Result<()> {
         let frontend_folder = opts.frontend_folder.clone();
 
         let mut app = App::new()
+        .wrap(
+            Cors::default()
+                .allow_any_origin()   // allows all origins, including localhost
+                .allow_any_method()   // allows GET, POST, etc.
+                .allow_any_header()   // allows all headers
+                .supports_credentials() // allow cookies/auth if needed
+                .max_age(3600),         // preflight cache duration
+        )
         .service(web::resource("/health").to(health))
         // Enable GZIP compression
         .wrap(middleware::Compress::default())
